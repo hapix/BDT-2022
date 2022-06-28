bed226945f943ea23464668164fdcd5959424797
hapix <itsc23@yahoo.com>
2022-06-28 18:01:15 +0200

    Add files via upload

--- Codes/Data_ingestion.py
+++ Codes/Data_ingestion.py
@@ -7,10 +7,16 @@ class Data_ingestion:
 
     def rmv_sensitive(self, data):
         self.data = data
-        data = data.drop(['id', 'name', 'email', 'address', 'phone', 'location'], axis=1)
+        data = data.drop(['id', 'name', 'email', 'address',
+                          'phone', 'location'], axis=1)
         data.rename(columns={'exp': 'label'}, inplace=True)
         data_dict = data.to_dict("records")
         return data_dict
 
-    def conv2json(self):
-        pass
+    def rmv_sensitive_csv(self, data):
+        self.data = data
+        data = data.drop(['ID', 'Firstname', 'Surname',
+                          'Email', 'Urban classification', 'Location'], axis=1)
+        data.rename(columns={'Expenditure': 'label'}, inplace=True)
+        data_dict = data.to_dict("records")
+        return data_dict
