2ad807126215fa4967180b48040bbac2762b5ad7
hapix <itsc23@yahoo.com>
2022-07-01 15:39:36 +0200

    Add files via upload

---
+++ Codes/API_handler.py
@@ -0,0 +1,19 @@
+import requests
+
+
+class API_handler:
+    def __init__(self, url = 'https://api.exchangerate-api.com/v4/latest/EUR'):
+        self.data = requests.get(url).json()
+        self.currencies = self.data['rates']
+
+    def convert(self, from_currency, to_currency, amount):
+        initial_amount = amount
+        if from_currency != 'EUR':
+            amount = amount / self.currencies[from_currency]
+
+            # limiting the precision to 4 decimal places
+        amount = round(amount * self.currencies[to_currency], 4)
+        return amount
+
+
+
